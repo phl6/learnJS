164d0af24caea68fb8c1554895813aa5ec0f7e12
phl6 <louisli1997@gmail.com>
2020-10-20 21:56:34 +0800

    added getElementById js code and added html skeleton

---
+++ src/script.js
@@ -0,0 +1,5 @@
+const { title } = require("process");
+
+let titleElement = document.getElementById('title');
+console.log(titleElement, titleElement instanceof HTMLElement);
+console.log(titleElement instanceof HTMLElement);
